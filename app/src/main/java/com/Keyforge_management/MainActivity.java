717a957717c6fe82b659358add2dc3cc4d1c13e0
Luca Pivetta <lucapivetta01@gmail.com>
2019-12-19 21:05:33 +0100

    added some graphics and started the layout of the main activity

--- app/src/main/java/com/Keyforge_management/MainActivity.java
+++ app/src/main/java/com/Keyforge_management/MainActivity.java
@@ -1,8 +1,9 @@
 package com.Keyforge_management;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 }
