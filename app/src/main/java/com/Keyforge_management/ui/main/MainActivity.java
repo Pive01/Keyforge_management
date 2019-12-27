8c082aabfdd346081e6f53e4a93a1b19826f67ea
Luca Pivetta <lucapivetta01@gmail.com>
2019-12-27 13:26:52 +0100

    added menu to the mainActivity  UI

--- app/src/main/java/com/Keyforge_management/ui/main/MainActivity.java
+++ app/src/main/java/com/Keyforge_management/ui/main/MainActivity.java
@@ -2,7 +2,10 @@ package com.Keyforge_management.ui.main;
 
 import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.Keyforge_management.R;
 import com.Keyforge_management.data.model.Deck;
@@ -15,7 +18,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -30,11 +32,7 @@ public class MainActivity extends AppCompatActivity implements DeckListInteracti
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        setSupportActionBar(findViewById(R.id.toolbar));
 
         View fab = findViewById(R.id.fab);
         fab.setOnClickListener(v -> SearchActivity.start(MainActivity.this));
@@ -59,6 +57,7 @@ public class MainActivity extends AppCompatActivity implements DeckListInteracti
         });
     }
 
+    @Override
     public void onDeckClicked(Deck deck) {
         new AlertDialog.Builder(this)
                 .setTitle("Remove a deck")
@@ -66,5 +65,29 @@ public class MainActivity extends AppCompatActivity implements DeckListInteracti
                 .setPositiveButton(android.R.string.yes, (dialog, which) -> repository.delete(deck))
                 .setNegativeButton(android.R.string.no, null)
                 .show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.scan_decks:
+                System.out.println("item 1 pressed");
+                Toast.makeText(MainActivity.this, "Scan deck not yet implemented", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_about_us:
+                Toast.makeText(MainActivity.this, "About us not yet implemented", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.search_deck:
+                SearchActivity.start(MainActivity.this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
