d6790b0ed2d34910ac394257618e49eb1a08fc6e
Luca Pivetta <lucapivetta01@gmail.com>
2020-02-23 13:54:15 +0100

    Remove unused code from card repository

--- app/src/main/java/com/KeyforgeManagement/application/data/storage/Card/CardRepository.java
+++ app/src/main/java/com/KeyforgeManagement/application/data/storage/Card/CardRepository.java
@@ -6,25 +6,16 @@ import com.KeyforgeManagement.application.data.model.Card;
 import com.KeyforgeManagement.application.data.storage.DecksDatabase;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.function.Consumer;
 
-import androidx.lifecycle.LiveData;
-
 public class CardRepository {
     private CardDao mCardDao;
-    private LiveData<List<Card>> mAllCards;
 
     public CardRepository(Context context) {
         DecksDatabase db = DecksDatabase.getDatabase(context);
         mCardDao = db.getCardDao();
-        mAllCards = mCardDao.getCards();
-    }
-
-    public LiveData<List<Card>> getAllCards() {
-        return mAllCards;
     }
 
     public void insert(Card card) {
