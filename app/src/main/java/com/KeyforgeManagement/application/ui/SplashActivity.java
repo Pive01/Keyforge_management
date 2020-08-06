9901f90c2c675794dc9dba71bbfd30209580a1bf
Luca Pivetta <lucapivetta01@gmail.com>
2020-08-06 23:45:11 +0200

    Simplify SplashActivity.java

--- app/src/main/java/com/KeyforgeManagement/application/ui/SplashActivity.java
+++ app/src/main/java/com/KeyforgeManagement/application/ui/SplashActivity.java
@@ -1,8 +1,6 @@
 package com.KeyforgeManagement.application.ui;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.KeyforgeManagement.application.R;
@@ -13,27 +11,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private ProgressBar loading;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        loading = findViewById(R.id.progress_bar_spalsh);
-        loading.setVisibility(View.VISIBLE);
-
         StatsRepository.refresh(getApplicationContext(), this::openMain, throwable -> failed());
     }
 
     private void failed() {
-        loading.setVisibility(View.GONE);
         Toast.makeText(this, "There has been an error while loading data", Toast.LENGTH_LONG).show();
         openMain();
     }
 
     private void openMain() {
         MainActivity.start(this);
-        loading.setVisibility(View.GONE);
     }
 }
